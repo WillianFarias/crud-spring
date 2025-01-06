832a91e5e27da4f93e157fe2369ad243354da67d
Willian Farias <williansousafarias@gmail.com>
2025-01-06 01:08:15 -0300

    Utilizacao do JsonProperty

--- src/main/java/com/loiane/model/Course.java
+++ src/main/java/com/loiane/model/Course.java
@@ -1,5 +1,6 @@
 package com.loiane.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,11 +14,12 @@ public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
     private Long id;
 
     @Column(length = 200, nullable = false)
     private String name;
 
-    @Column(length = 10, nullable = false)
+    @Column(length = 20, nullable = false)
     private String category;
 }
